03a57d324a16956c2db9d35ffc9834144f4fb50a
O Wooyoung <fz7948@gmail.com>
2021-08-24 13:21:13 +0900

    디렉토리 구조 정리

---
+++ src/onBoarding/components/AddFriends.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const AddFriends = () => {};
+
+export default AddFriends;
---
+++ src/onBoarding/index.js
@@ -0,0 +1,7 @@
+export { default as Splash } from './Splash';
+export { default as AddFavFood } from './components/AddFavFood';
+export { default as AddFriends } from './components/AddFriends';
+export { default as AddStyle } from './components/AddStyle';
+export { default as Login } from './components/Login';
+export { default as Signup } from './components/Signup';
+export { default as SignupComplete } from './components/SignupComplete';
