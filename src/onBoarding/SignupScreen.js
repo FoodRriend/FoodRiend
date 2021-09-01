98c8ac9e10b118ab65470ef11277dedcffd9fb98
O Wooyoung <fz7948@gmail.com>
2021-09-01 21:26:56 +0900

    babel.config.js로 절대경로 설정 / react-native-reanimated로 애니메이션 최적화

---
+++ src/onBoarding/SignupScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { Text, View, Pressable } from 'react-native';
+
+const SignupScreen = () => {
+  return (
+    <View>
+      <Text>SignupScreen</Text>
+      <Pressable>
+        <Text>Click</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+export default SignupScreen;
