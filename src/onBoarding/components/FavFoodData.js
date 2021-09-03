f82f4df21955262ce1aeb246f5707137ed331581
O Wooyoung <fz7948@gmail.com>
2021-09-03 20:21:04 +0900

    feat: [onBoarding/AddFavFoodScreen] 스타일링

---
+++ src/onBoarding/components/FavFoodData.js
@@ -0,0 +1,16 @@
+const favFoodData = [
+  { title: '술', url: '../assets/images/onBoading/favFood/grapes.png' },
+  { title: '커피', url: '../assets/images/onBoading/favFood/coffee.png' },
+  { title: '베이커리/디저트', url: '../assets/images/onBoading/favFood/pudding.png' },
+  { title: '해산물', url: '../assets/images/onBoading/favFood/octopus.png' },
+  { title: '치킨', url: '../assets/images/onBoading/favFood/friedChicken.png' },
+  { title: '피자', url: '../assets/images/onBoading/favFood/pizza.png' },
+  { title: '면', url: '../assets/images/onBoading/favFood/noodles.png' },
+  { title: '분식', url: '../assets/images/onBoading/favFood/tteokbokki.png' },
+  { title: '샐러드', url: '../assets/images/onBoading/favFood/salad.png' },
+  { title: '국밥', url: '../assets/images/onBoading/favFood/riceSoup.png' },
+  { title: '찌개/탕', url: '../assets/images/onBoading/favFood/stew.png' },
+  { title: '고기', url: '../assets/images/onBoading/favFood/chop.png' },
+];
+
+export default favFoodData;
