e72fc4f3204c1a62aee06619973bb44930dbb45d
O Wooyoung <fz7948@gmail.com>
2021-09-04 15:52:33 +0900

    feat: [onBoarding/SignupCompleteScreen] 스타일링

--- src/onBoarding/SignupCompleteScreen.js
+++ src/onBoarding/SignupCompleteScreen.js
@@ -11,15 +11,7 @@ const SignupCompleteScreen = () => {
     navigation.setOptions({
       headerShown: true,
       title: '회원가입',
-      headerLeft: () => (
-        <Pressable
-          style={styles.BackIcon}
-          onPress={() => {
-            navigation.navigate('Login');
-          }}>
-          <Image source={require(`../assets/icons/Left.png`)}></Image>
-        </Pressable>
-      ),
+      headerLeft: () => <></>,
     });
   };
 
@@ -27,7 +19,25 @@ const SignupCompleteScreen = () => {
 
   return (
     <Wrapper>
-      <Text>회원가입 완료</Text>
+      <Image
+        style={{ width: 390, height: 430 }}
+        resizeMode="cover"
+        source={require(`../assets/images/onBoading/signupComplete.png`)}
+      />
+      <SignupEndTextContainer>
+        <Text style={styles.signupEndTitle}>회원가입 완료</Text>
+        <View style={styles.signupEndContentBox}>
+          <Text style={styles.signupEndContent}>친구와 함께</Text>
+          <Text style={styles.signupEndContent}>맛있는 순간을 함께 해요!</Text>
+        </View>
+      </SignupEndTextContainer>
+      <Pressable
+        onPress={() => {
+          navigation.navigate('Login');
+        }}
+        style={styles.signupEndButton}>
+        <Text style={styles.signupEndButtonText}>이용하러 가기</Text>
+      </Pressable>
     </Wrapper>
   );
 };
@@ -44,13 +54,60 @@ const styles = StyleSheet.create({
     paddingLeft: 13,
     paddingRight: 20,
   },
+  signupEndTitle: {
+    width: 129,
+    height: 32,
+    fontSize: 22,
+    fontWeight: 'bold',
+    fontStyle: 'normal',
+    color: '#2e3e5c',
+    paddingLeft: 3,
+  },
+  signupEndContentBox: {
+    width: 200,
+    height: 50,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 20,
+  },
+  signupEndContent: {
+    fontSize: 17,
+    fontWeight: '500',
+    fontStyle: 'normal',
+    color: '#7e8389',
+  },
+  signupEndButton: {
+    marginTop: 38,
+    width: 327,
+    height: 56,
+    borderRadius: 32,
+    backgroundColor: '#fe554a',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  signupEndButtonText: {
+    fontSize: 15,
+    fontWeight: '900',
+    fontStyle: 'normal',
+    color: '#ffffff',
+  },
 });
 
 const Wrapper = styled.View({
-  paddingTop: 36,
+  paddingTop: 12,
   backgroundColor: '#fff',
   width: '100%',
   height: '100%',
   display: 'flex',
+  alignItems: 'center',
+});
+
+const SignupEndTextContainer = styled.View({
+  width: 390,
+  height: 160,
+  display: 'flex',
+  justifyContent: 'center',
   alignItems: 'center',
 });
