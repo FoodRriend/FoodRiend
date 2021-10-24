cee145dc7df9c1955b2d9cb1539e106badd91896
fz7948 <fz7948@gmail.com>
2021-10-24 23:40:51 +0900

    fixed untracked files

---
+++ ios/Pods/Target Support Files/Pods-FoodRiend_client-FoodRiend_clientTests/Pods-FoodRiend_client-FoodRiend_clientTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FoodRiend_client_FoodRiend_clientTests : NSObject
+@end
+@implementation PodsDummy_Pods_FoodRiend_client_FoodRiend_clientTests
+@end
